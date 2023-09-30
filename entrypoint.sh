65016280321a86e3ad6931deee1fd1e801fcb702
Naor Yamin <naory902@gmail.com>
2023-09-30 17:46:06 +0300

    fixed entrypoint.sh file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
 
-echo "Hello $INPUT_MYINPUT   
+echo "Hello $INPUT_MYINPUT"   
 memory=$(cat /proc/meminfo)      # gets the current memory and sets it as an output variable that actions running later in a job can use.
-echo "::set-output name=memory::$memory" # sets the action's output parameter using the workflow syntax
+echo "::set-output name=memory::$memory" #sets the action's output parameter using the workflow syntax
